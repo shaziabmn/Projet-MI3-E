43536e3e7310f11793ca8ca2b4a2b3f0b243195e
Shazia <shazia95490@gmail.com>
2025-05-01 20:38:15 +0200

    Create combat.h

---
+++ combat.h
@@ -0,0 +1,37 @@
+#ifndef COMBAT_H
+#define COMBAT_H
+
+#define NAME_SIZE 20
+#define MAX_FIGHTERS 5
+#define MAX_TECHNIQUES 5
+
+//Structure Technique
+typedef struct {
+    char name[NAME_SIZE];
+    int value;
+    char description;
+    int active_turns;
+    int reload_turns;
+} Technique;
+
+//Structure Fighter
+typedef struct {
+    char name[NAME_SIZE];
+    int currents_hp;
+    int max_hp;
+    int attack;
+    int defense;
+    int agility;
+    int speed;
+    Technique special[MAX_TECHNIQUES];
+    int technique_count;
+} Fighter;
+
+//Structure Team
+typedef struct {
+    char name[NAME_SIZE];
+    Fighter members[MAX_FIGHTERS];
+} Team;
+
+#endif
+
