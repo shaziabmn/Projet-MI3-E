a5766ad29074d74e11ec8da8085feff02f9e5a84
Shazia <shazia95490@gmail.com>
2025-05-02 13:55:52 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 //Test des fonctions dans le main
 
 int main() {
-Fighter hiba = {"Hiba", 100, 100, 20, 5, 10, 10};
-Fighter bot = {"Robot", 80, 80, 15, 3, 8, 8};
+Fighter Player = {"Player", 100, 100, 20, 5, 10, 10};
+Fighter bot2 = {"Robot", 80, 80, 15, 3, 8, 8};
 Technique poing = {"Coup de poing", 10, "attaque", 0, 0};
 
-utiliser_technique(&hiba, &bot, poing);
+utiliser_technique(&Player, &bot2, poing);
 printf("PV restants du Robot : %d\n", bot.pv_currents);
 
 return 0;
