7f3df955978e3d85c166c7a82c8585281f075ba5
Shazia <shazia95490@gmail.com>
2025-05-02 13:12:24 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,51 @@
+//Fonction utiliser technique
 
+void utiliser_technique(Fighter *attaquant, Fighter *cible, Technique tech) {
+// Exemple par rapport à la description
+if (strcmp(tech.description, "attaque") == 0) {
+attaquer(attaquant, cible, tech);
+}
+else if (strcmp(tech.description, "soin") == 0) {
+attaquant->pv_currents += tech.value;
+if (attaquant->pv_currents > attaquant->pv_max) {
+attaquant->pv_currents = attaquant->pv_max;
+}
+}
+else if (strcmp(tech.description, "recharge") == 0) {
+// Peut être gérer de l'énergie (dans future version)
+printf("%s se recharge\n", attaquant->name);
+}
+
+}
+
+//Fonction attaquer
+
+int attaquer(Fighter *attaquant, Fighter *cible, Technique tech) {
+// Calcul dégâts
+int degats = tech.value + attaquant->attack - cible->defense;
+
+// Empêcher dégâts négatifs
+if (degats < 0) degats = 0;
+
+// Appliquer dégâts
+cible->pv_currents -= degats;
+
+// Empêcher PV négatifs
+if (cible->pv_currents < 0) cible->pv_currents = 0;
+
+// Retour PV restants
+return cible->pv_currents;
+}
+
+//Test des fonctions dans le main
+
+int main() {
+Fighter hiba = {"Hiba", 100, 100, 20, 5, 10, 10};
+Fighter bot = {"Robot", 80, 80, 15, 3, 8, 8};
+Technique poing = {"Coup de poing", 10, "attaque", 0, 0};
+
+utiliser_technique(&hiba, &bot, poing);
+printf("PV restants du Robot : %d\n", bot.pv_currents);
+
+return 0;
+}
