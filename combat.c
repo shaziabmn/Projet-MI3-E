bc854ab439a6bee80e982ad961362f52d247bea5
Shazia <shazia95490@gmail.com>
2025-05-02 13:53:03 +0200

    Update combat.c

--- combat.c
+++ combat.c
@@ -11,9 +11,9 @@ if (strcmp(tech.description, "attaque") == 0) {
 attaquer(attaquant, cible, tech);
 }
 else if (strcmp(tech.description, "soin") == 0) {
-attaquant->pv_currents += tech.value;
-if (attaquant->pv_currents > attaquant->pv_max) {
-attaquant->pv_currents = attaquant->pv_max;
+attaquant->currents_hp += tech.value;
+if (attaquant->currents_hp > attaquant->max_hp) {
+attaquant->currents_hp = attaquant->max_hp;
 }
 }
 else if (strcmp(tech.description, "recharge") == 0) {
@@ -33,11 +33,11 @@ int degats = tech.value + attaquant->attack - cible->defense;
 if (degats < 0) degats = 0;
 
 // Appliquer dégâts
-cible->pv_currents -= degats;
+cible->currents_hp -= degats;
 
-// Empêcher PV négatifs
-if (cible->pv_currents < 0) cible->pv_currents = 0;
+// Empêcher HP négatifs
+if (cible->currents_hp < 0) cible->currents_hp = 0;
 
-// Retour PV restants
-return cible->pv_currents;
+// Retour HP restants
+return cible->currents_hp;
 }
