111cd5371e573b87b57840ceb8a8b8661d1519c5
Shazia <shazia95490@gmail.com>
2025-05-07 18:46:20 +0200

    Update combat.c

--- combat.c
+++ combat.c
@@ -1,3 +1,77 @@
+//PARTIE SHAZIA 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "combat.h"
+
+// Fonction pour charger les combattants depuis un fichier texte
+int charger_combattants(const char *nomFichier, Combattant liste[], int max) {
+    FILE *f = fopen(nomFichier, "r");  // Ouvre le fichier en mode lecture
+
+     // Si le fichier ne peut pas être ouvert, afficher une erreur
+    if (!f) {                        
+        perror("Erreur ouverture fichier"); 
+        return 0;                      
+    }
+
+    char ligne[256];  // Pour lire chaque ligne du fichier
+    int nb = 0;       // Compteur de combattants lus
+
+    // Lire les lignes tant qu'on n'a pas atteint la fin
+    while (fgets(ligne, sizeof(ligne), f) && nb < max) {
+
+        Combattant *c = &liste[nb];  /// Récupère un pointeur vers le prochain combattant à remplir
+
+        // Lire et extraire les données formatées depuis la ligne
+        sscanf(ligne, " %[^;] ; %d ; %d ; %d ; %d ; %d ; %[^;] ; %d ; %[^;] ; %d ; %d",
+            c->nom, &c->pv_max, &c->attaque, &c->defense,
+            &c->agilite, &c->vitesse,
+            c->special.nom, &c->special.valeur, c->special.description,
+            &c->special.tours_actifs, &c->special.tours_rechargement);     
+
+        c->pv_courants = c->pv_max;  // Initialiser les PV courants au maximum
+        nb++;                        // Passer au prochain combattant
+
+        }
+    fclose(f);   // Fermer le fichier
+    return nb;   
+}
+
+
+// Fonction pour afficher les caractéristiques d’un combattant
+void afficher_combattant(const Combattant *c){
+    // Affiche les informations générales
+    printf("Nom : %s\n", c->nom);
+    printf("PV : %d/%d\n", c->pv_courants, c->pv_max);
+    printf("Attaque : %d | Défense : %d | Agilité : %d | Vitesse : %d\n",
+           c->attaque, c->defense, c->agilite, c->vitesse);
+
+    // Affiche les détails de la technique spéciale
+    printf("Technique spéciale : %s (%s)\n", c->special.nom, c->special.description);
+    printf("Valeur : %d | Actif : %d tours | Rechargement : %d tours\n",
+           c->special.valeur, c->special.tours_actifs, c->special.tours_rechargement);
+
+    printf("-----------------------------\n");  // Ligne de séparation pour plus de clarté
+    }
+
+
+// Fonction pour vérifier si un combattant est KO
+int est_ko(const Combattant *c){
+    return c->pv_courants <= 0;  // Retourne 1 si KO, 0 sinon
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//PARTIE HIBA 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
