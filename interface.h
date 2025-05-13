b330b913ad7da3cd73f82a11cb121753c3f922b7
Shazia <shazia95490@gmail.com>
2025-05-13 21:07:49 +0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,13 +1,15 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
 
-#include "combat.h"  // Pour utiliser les structures Combattant
 #include <stdio.h>
 #include <string.h>
 
-// Couleurs ANSI pour l'affichage dans le terminal
-#define RESET       "\033[0m"
-#define BOLD        "\033[1m"
+#include "combat.h"  
+
+
+// Définition des codes de couleurs ANSI pour l'affichage dans le terminal
+#define RESET       "\033[0m" // Réinitialise la couleur à la valeur par défaut
+#define BOLD        "\033[1m" // Met le texte en gras
 #define RED         "\033[31m"
 #define GREEN       "\033[32m"
 #define YELLOW      "\033[33m"
@@ -15,20 +17,28 @@
 #define MAGENTA     "\033[35m"
 #define CYAN        "\033[36m"
 #define WHITE       "\033[37m"
-#define BOLD_YELLOW "\033[1;33m"
-#define BOLD_CYAN   "\033[1;36m"
-#define BOLD_GREEN  "\033[1;32m"
-#define BOLD_RED    "\033[1;31m"
-#define BOLD_WHITE  "\033[1;37m"
-#define BOLD_BLUE   "\033[1;34m"
 
+// Met le texte en couleurs et en gras
+#define BOLD_RED    "\033[1;31m"
+#define BOLD_GREEN  "\033[1;32m"
+#define BOLD_YELLOW "\033[1;33m"
+#define BOLD_BLUE   "\033[1;34m"
+#define BOLD_MAGENTA "\033[1;35m"
+#define BOLD_CYAN   "\033[1;36m"
+#define BOLD_WHITE  "\033[1;37m"
+
+
+// Définition de la largeur du terminal pour centrer des éléments à l'écran
 #define LARGEUR_TERMINAL 80
 
-// Fonctions graphiques
-void print_centered_color(const char* texte, const char* couleur);
-void afficher_lobby();
-void afficher_menu();
-void affichage_perso(const Combattant *perso);
-void afficher_equipes(Joueur *j1, Joueur *j2);
+
+// Déclarations des fonctions
+void afficher_centre(const char* texte, const char* couleur); // Afficher un texte centré avec une couleur
+void afficher_jeu(); // Afficher l'écran d'accueil du jeu
+void afficher_menu(); // Afficher le menu principal
+void afficher_perso(const Combattant *perso); // Afficher les caractéristiques des combattants
+void afficher_liste_combattants(Combattant *liste, int n); // Afficher la liste des combattants
+void afficher_equipes(Equipe *e1, Equipe *e2); // Afficher les deux équipes sélectionées
+
 
 #endif
